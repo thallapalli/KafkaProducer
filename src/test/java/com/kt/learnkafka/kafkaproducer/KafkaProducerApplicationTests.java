4fa43f8dd5e7b7d18968a26adeefc7b988f4ebef
karnakar <karnakar@ymail.com>
2020-06-21 03:58:33 -0400

    Junit Test case added

--- src/test/java/com/kt/learnkafka/kafkaproducer/KafkaProducerApplicationTests.java
+++ src/test/java/com/kt/learnkafka/kafkaproducer/KafkaProducerApplicationTests.java
@@ -1,13 +1,43 @@
 package com.kt.learnkafka.kafkaproducer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.kt.learnkafka.kafkaproducer.domian.Book;
+import com.kt.learnkafka.kafkaproducer.domian.Event;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 class KafkaProducerApplicationTests {
+	@Autowired 
+	TestRestTemplate testRestTempalte;
+	
+	
 
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void  testpostEvent( ) {
+		Book book=Book.builder().bookId(1).bookName("Name").bookAuthor("Auth").build();
+		Event event=Event.builder().eventId(2).book(book).build();
+		
+		HttpHeaders httpHeader=new HttpHeaders();
+		httpHeader.set("contect-type",MediaType.APPLICATION_JSON_VALUE);
+		HttpEntity<Event> request=new HttpEntity<>(event,httpHeader);
+		ResponseEntity<Event> exchange = testRestTempalte.exchange("/v1/event", HttpMethod.POST, request, Event.class);
+		assertEquals(HttpStatus.CREATED, exchange.getStatusCode());
+		
+	}
+	
 }
