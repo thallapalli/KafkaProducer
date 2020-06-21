6c7e3ca2278d1fc3ba126c54f0ed12fee9d4d8c9
karnakar <karnakar@ymail.com>
2020-06-21 13:36:40 -0400

    Junit test with mockito added

---
+++ src/test/java/com/kt/learnkafka/kafkaproducer/controller/KafkaProducerControllerTest.java
@@ -0,0 +1,38 @@
+package com.kt.learnkafka.kafkaproducer.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kt.learnkafka.kafkaproducer.domian.Book;
+import com.kt.learnkafka.kafkaproducer.domian.Event;
+import com.kt.learnkafka.kafkaproducer.domian.EventType;
+
+@WebMvcTest(KafkaProducerController.class)
+@AutoConfigureMockMvc
+public class KafkaProducerControllerTest {
+	@Autowired
+	MockMvc mockkMvc;
+	ObjectMapper objMapper=new ObjectMapper();
+	@Test
+	void postEventTest() throws Exception {
+		Book book = Book.builder().bookId(1).bookName("Name").bookAuthor("Auth").build();
+		Event event = Event.builder().eventId(2).book(book).ventType(EventType.NEW).build();
+		String payload = objMapper.writeValueAsString(event);
+		mockkMvc.perform(post("/v1/event")
+				.content(payload)
+				.contentType(MediaType.APPLICATION_JSON)
+				
+				
+				).andExpect(status().isCreated());
+		
+	}
+	
+}
