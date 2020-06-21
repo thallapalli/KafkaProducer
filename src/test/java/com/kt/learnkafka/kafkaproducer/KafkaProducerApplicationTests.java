d2d87a40d68ff4bf27fa1531aaa99c22997d2885
karnakar <karnakar@ymail.com>
2020-06-21 04:59:50 -0400

    Producer added for test case

--- src/test/java/com/kt/learnkafka/kafkaproducer/KafkaProducerApplicationTests.java
+++ src/test/java/com/kt/learnkafka/kafkaproducer/KafkaProducerApplicationTests.java
@@ -2,7 +2,18 @@ package com.kt.learnkafka.kafkaproducer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,39 +23,85 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.Timed;
 import org.springframework.test.context.TestPropertySource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kt.learnkafka.kafkaproducer.domian.Book;
 import com.kt.learnkafka.kafkaproducer.domian.Event;
+import com.kt.learnkafka.kafkaproducer.domian.EventType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(topics= {"event"})
-@TestPropertySource(properties= {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
-		
+@EmbeddedKafka(topics = { "event" })
+@TestPropertySource(properties = { "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+
 		"spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
 
 })
 class KafkaProducerApplicationTests {
-	@Autowired 
+	@Autowired
 	TestRestTemplate testRestTempalte;
-	
-	
+	@Autowired
+	EmbeddedKafkaBroker embeddedKafkaBroker;
+	@Autowired
+	ObjectMapper objMapper;
+
+	private Consumer<Integer, String> consumer;
+
+	@BeforeEach
+	void setUp() {
+		// String group, String autoCommit,
+		// EmbeddedKafkaBroker embeddedKafka
+
+		Map<String, Object> configs = new HashMap<>(
+				KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+		Deserializer keyDeserializerSupplier = new IntegerDeserializer();
+		Deserializer valueDeserializerSupplier = new StringDeserializer();
+		consumer = new DefaultKafkaConsumerFactory<>(configs, keyDeserializerSupplier, valueDeserializerSupplier)
+				.createConsumer();
+		embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+
+	}
+
+	@AfterEach
+	void tearDown() {
+		consumer.close();
+	}
 
 	@Test
 	void contextLoads() {
 	}
+
 	@Test
-	public void  testpostEvent( ) {
-		Book book=Book.builder().bookId(1).bookName("Name").bookAuthor("Auth").build();
-		Event event=Event.builder().eventId(2).book(book).build();
-		
-		HttpHeaders httpHeader=new HttpHeaders();
-		httpHeader.set("contect-type",MediaType.APPLICATION_JSON_VALUE);
-		HttpEntity<Event> request=new HttpEntity<>(event,httpHeader);
+	@Timeout(5)
+	public void testpostEvent() {
+		Book book = Book.builder().bookId(1).bookName("Name").bookAuthor("Auth").build();
+		Event event = Event.builder().eventId(2).book(book).ventType(EventType.NEW).build();
+
+		HttpHeaders httpHeader = new HttpHeaders();
+		httpHeader.set("contect-type", MediaType.APPLICATION_JSON_VALUE);
+		HttpEntity<Event> request = new HttpEntity<>(event, httpHeader);
 		ResponseEntity<Event> exchange = testRestTempalte.exchange("/v1/event", HttpMethod.POST, request, Event.class);
 		assertEquals(HttpStatus.CREATED, exchange.getStatusCode());
+		ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, "event");
+
+		try {
+			
+
+			String expected = objMapper.writeValueAsString(event);
+			String value = singleRecord.value();
+			assertEquals(expected, value);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
+
 	}
-	
+
 }
